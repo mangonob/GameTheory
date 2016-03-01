72696047e2e3a4ece041ea563cb67437a7c49307
mangonob <534393806@qq.com>
2016-03-01 23:23:04 +0800

    Initial Commit

---
+++ GameTheory/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  GameTheory
+//
+//  Created by Trinity on 16/3/1.
+//  Copyright © 2016年 Trinity. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
